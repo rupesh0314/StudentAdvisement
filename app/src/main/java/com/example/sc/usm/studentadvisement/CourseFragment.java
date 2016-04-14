5d87b3575bf774c6ae2daf8f00024e085ccaa0f4
rupesh0314 <rupesh.0314@gmail.com>
2016-04-13 23:32:48 -0500

    First Commit
    added tabs in navigation bar and fragments made

---
+++ app/src/main/java/com/example/sc/usm/studentadvisement/CourseFragment.java
@@ -0,0 +1,52 @@
+package com.example.sc.usm.studentadvisement;
+
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CourseFragment extends Fragment, ListFragment {
+
+
+    public CourseFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        ViewGroup rootview=(ViewGroup)inflater.inflate(R.layout.fragment_course, container, false);
+        //return inflater.inflate(R.layout.fragment_course, container, false);
+        String[] courses={"Computer Science","ITC"};
+        //create adapter
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(getActivity(),R.layout.fragment_cs,R.id.txtitem,courses);
+        //bind adapter to list fragment
+        setListAdapter(adapter);
+        //Reatin list fragment instance across  configuration changes
+        setRetainInstance(true);
+        return rootview;
+
+    }
+    //handling itemclick
+    public void onListItemClick(ListView l, View view, int position, long id){
+        ViewGroup viewGroup=(ViewGroup)view;
+        TextView txt=(TextView)viewGroup.findViewById(R.id.txtitem);
+        Toast.makeText(getActivity(),txt.getText().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+
+}
