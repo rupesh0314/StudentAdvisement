6fa240bdc12ec416606a3f5d29db89fba0f1cafe
rupesh0314 <rupesh.0314@gmail.com>
2016-04-17 22:59:36 -0500

    formatted code

--- app/src/main/java/com/example/sc/usm/studentadvisement/FacultyFragment.java
+++ app/src/main/java/com/example/sc/usm/studentadvisement/FacultyFragment.java
@@ -1,30 +1,20 @@
 package com.example.sc.usm.studentadvisement;
 
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.ListFragment;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import com.example.sc.usm.studentadvisement.R;
-import java.util.List;
 
 
 public class FacultyFragment extends ListFragment {
-    String[] faculty_list={"Adams,Todd",
+    String[] faculty_list = {"Adams,Todd",
             "Ali, Dr.Dia",
             "Banerjee,Dr.Bikramjit",
             "Chen, Dr.Kuo",
@@ -42,36 +32,37 @@ public class FacultyFragment extends ListFragment {
             "Robinson, Ms.Kelly",
             "Sun, Dr.Jonathan",
             "Sung, Dr.Andrew",
-          "Vital, Mr.Dennis",
+            "Vital, Mr.Dennis",
             "Wang, Dr.Nan",
             "Wang, Dr.Zheng",
             "Xu, Dr.Kebin",
             "Zhang, Dr.Joe",
             "Zhou, Dr.Zhaoxian",
-           "Davis, Mr.Penny",
+            "Davis, Mr.Penny",
             "Sherry, Mrs.Smith"
+
     };
 
 
-    int[] faculty_images ={R.drawable.toddadams,
-    R.drawable.diaali,
+    int[] faculty_images = {R.drawable.toddadams,
+            R.drawable.diaali,
             R.drawable.bikramjitbanerjee,
-                    R.drawable.kuochen,
-                    R.drawable.jameslclark,
-                    R.drawable.amerdawoud,
-                    R.drawable.danielpgarcia,
-                    R.drawable.jeremygraves,
-                    R.drawable.rickhawkins,
-                    R.drawable.johnhheath,
-                    R.drawable.wonryullkoh,
+            R.drawable.kuochen,
+            R.drawable.jameslclark,
+            R.drawable.amerdawoud,
+            R.drawable.danielpgarcia,
+            R.drawable.jeremygraves,
+            R.drawable.rickhawkins,
+            R.drawable.johnhheath,
+            R.drawable.wonryullkoh,
             R.drawable.sean,
-                    R.drawable.beddhumurali,
+            R.drawable.beddhumurali,
             R.drawable.rahaim,
             R.drawable.tomrishel,
-                    R.drawable.kellyrobinson,
+            R.drawable.kellyrobinson,
             R.drawable.jonathansun,
             R.drawable.andrewhsung,
-          R.drawable.vital,
+            R.drawable.vital,
             R.drawable.nanwang,
             R.drawable.zhengwang,
             R.drawable.kebinxu,
@@ -80,11 +71,11 @@ public class FacultyFragment extends ListFragment {
             R.drawable.penny,
             R.drawable.sherrysmith
 
+
     };
 
-    ArrayList<HashMap<String,String>> data=new ArrayList<HashMap<String, String>>();
+    ArrayList<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
     SimpleAdapter simpleAdapter;
-
 
 
     public FacultyFragment() {
@@ -94,21 +85,19 @@ public class FacultyFragment extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        HashMap<String,String> map=new HashMap<String,String>();
+        HashMap<String, String> map = new HashMap<String, String>();
 
-        for (int i=0;i<faculty_list.length ; i++)
-        {
-            map=new HashMap<String,String>();
-            map.put("faculty_list",faculty_list[i]);
-            map.put("faculty_images",Integer.toString(faculty_images[i]));
+        for (int i = 0; i <faculty_list.length; i++) {
+            map = new HashMap<String, String>();
+            map.put("faculty_list", faculty_list[i]);
+            map.put("faculty_images", Integer.toString(faculty_images[i]));
             data.add(map);
 
         }
-        String[] from={"name","image"};
-        int[] to={R.id.faculty_name,R.id.faculty_image};
-        simpleAdapter=new SimpleAdapter(getActivity(),data,R.layout.faculty_list,from,to);
+        String[] from = {"faculty_list", "faculty_images"};
+        int[] to = {R.id.faculty_name, R.id.faculty_image};
+        simpleAdapter = new SimpleAdapter(getActivity(), data, R.layout.faculty_list, from, to);
         setListAdapter(simpleAdapter);
-
 
 
         return super.onCreateView(inflater, container, savedInstanceState);
@@ -124,12 +113,12 @@ public class FacultyFragment extends ListFragment {
         getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getActivity(),data.get(position).get("name"),Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), data.get(position).get("name"), Toast.LENGTH_SHORT).show();
 
             }
 
-           // FragmentTransaction fragmentTransaction= getActivity().getSupportFragmentManager().beginTransaction();
-           // fragmentTransaction.replace(R.id.fragment_container, fragment);
+            // FragmentTransaction fragmentTransaction= getActivity().getSupportFragmentManager().beginTransaction();
+            // fragmentTransaction.replace(R.id.fragment_container, fragment);
             //fragmentTransaction.commit();
         });
 
